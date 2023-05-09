980d611ccfc0c6549aec33bff5db2b361dd252ec
Straumann Till <till.straumann@psi.ch>
2023-05-09 16:32:10 +0200

    cpsi_cio_top.vhd: cleanup

--- hdl/cpsi_cio_top.vhd
+++ hdl/cpsi_cio_top.vhd
@@ -11,6 +11,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 library UNISIM;
 use UNISIM.VCOMPONENTS.ALL;
+use work.psi_common_axi_pkg.all;
+
 entity cpsi_cio_top is
  generic (
    G_FW_GIT_VERSION      : std_logic_vector(255 downto 0) := (others=>'1');
@@ -101,189 +103,29 @@ entity cpsi_cio_top is
     L8_MSH_GBT_RX_P     : in STD_LOGIC;
     L8_MSH_GBT_TX_N     : out STD_LOGIC;
     L8_MSH_GBT_TX_P     : out STD_LOGIC
-  );end cpsi_cio_top;
+  );
+end entity cpsi_cio_top;
 
 architecture STRUCTURE of cpsi_cio_top is
-  component cpsi_cio is
-      port (
-        param_fw_git_version_i_0    : in STD_LOGIC_VECTOR ( 255 downto 0 );
-        param_fw_build_datetime_i_0 : in STD_LOGIC_VECTOR ( 159 downto 0 );
-        CLK_IN_D_0_clk_p            : in STD_LOGIC_VECTOR ( 4 downto 0 );
-        CLK_IN_D_0_clk_n            : in STD_LOGIC_VECTOR ( 4 downto 0 );
-        CLK_A_FPGA_DP_OUT_P         : out STD_LOGIC;
-        CLK_A_FPGA_DP_OUT_N         : out STD_LOGIC;
-        MGT_UPLINK_RX_P : in STD_LOGIC;
-        MGT_UPLINK_RX_N : in STD_LOGIC;
-        MGT_UPLINK_TX_P : out STD_LOGIC;
-        MGT_UPLINK_TX_N : out STD_LOGIC;
-        MGT_UPLINK_RECCLK_P : out STD_LOGIC_VECTOR ( 0 to 0 );
-        MGT_UPLINK_RECCLK_N : out STD_LOGIC_VECTOR ( 0 to 0 );
-        TRIGGER_OUT        : out STD_LOGIC_VECTOR ( 1 downto 0 );
-        fp_lemo_ena_n       : out STD_LOGIC_VECTOR ( 1 downto 0 );
-        fp_lemo_in          : in STD_LOGIC_VECTOR ( 1 downto 0 );
-        i_fanout_mgt_qsfp0_rx_p : in STD_LOGIC_VECTOR ( 3 downto 0 );
-        i_fanout_mgt_qsfp0_rx_n : in STD_LOGIC_VECTOR ( 3 downto 0 );
-        o_fanout_mgt_qsfp0_tx_p : out STD_LOGIC_VECTOR ( 3 downto 0 );
-        o_fanout_mgt_qsfp0_tx_n : out STD_LOGIC_VECTOR ( 3 downto 0 );
 
-        i_fanout_mgt_qsfp1_rx_p : in STD_LOGIC_VECTOR ( 3 downto 0 );
-        i_fanout_mgt_qsfp1_rx_n : in STD_LOGIC_VECTOR ( 3 downto 0 );
-        o_fanout_mgt_qsfp1_tx_p : out STD_LOGIC_VECTOR ( 3 downto 0 );
-        o_fanout_mgt_qsfp1_tx_n : out STD_LOGIC_VECTOR ( 3 downto 0 );
-
-        i_fanout_mgt_p6_8765_rx_p : in STD_LOGIC_VECTOR ( 3 downto 0 );
-        i_fanout_mgt_p6_8765_rx_n : in STD_LOGIC_VECTOR ( 3 downto 0 );
-        o_fanout_mgt_p6_8765_tx_p : out STD_LOGIC_VECTOR ( 3 downto 0 );
-        o_fanout_mgt_p6_8765_tx_n : out STD_LOGIC_VECTOR ( 3 downto 0 );
-
-        i_fanout_mgt_p6_432_rx_p : in  STD_LOGIC_VECTOR ( 3 downto 0 );
-        i_fanout_mgt_p6_432_rx_n : in  STD_LOGIC_VECTOR ( 3 downto 0 );
-        o_fanout_mgt_p6_432_tx_p : out STD_LOGIC_VECTOR ( 3 downto 0 );
-        o_fanout_mgt_p6_432_tx_n : out STD_LOGIC_VECTOR ( 3 downto 0 )
-      );
-  end component cpsi_cio;
-  signal CLK_IN_D_0_clk_n : std_logic_vector(4 downto 0);
-  signal CLK_IN_D_0_clk_p : std_logic_vector(4 downto 0);
-
-  signal slv_fanout_mgt_qsfp0_rx_p : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_qsfp0_rx_n : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_qsfp0_tx_p : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_qsfp0_tx_n : std_logic_vector(3 downto 0);
-
-  signal slv_fanout_mgt_qsfp1_rx_p : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_qsfp1_rx_n : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_qsfp1_tx_p : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_qsfp1_tx_n : std_logic_vector(3 downto 0);
-
-  signal slv_fanout_mgt_p6_8765_rx_p : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_p6_8765_rx_n : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_p6_8765_tx_p : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_p6_8765_tx_n : std_logic_vector(3 downto 0);
-
-  signal slv_fanout_mgt_p6_432_rx_p : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_p6_432_rx_n : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_p6_432_tx_p : std_logic_vector(3 downto 0);
-  signal slv_fanout_mgt_p6_432_tx_n : std_logic_vector(3 downto 0);
+   signal axiClk       : std_logic;
+   signal axiRst       : std_logic;
+   signal axi_ms       : rec_axi_ms;
+   signal axi_sm       : rec_axi_sm;
+   signal irq          : std_logic_vector(15 downto 0);
 
 begin
-CLK_IN_D_0_clk_n(0) <= CLK_A_GTH_MSH_SFP1_N;
-CLK_IN_D_0_clk_p(0) <= CLK_A_GTH_MSH_SFP1_P;
-CLK_IN_D_0_clk_n(1) <= CLK_A_GTH_QSFP0_N;
-CLK_IN_D_0_clk_p(1) <= CLK_A_GTH_QSFP0_P;
-CLK_IN_D_0_clk_n(2) <= CLK_B_GTH_MSH_N;
-CLK_IN_D_0_clk_p(2) <= CLK_B_GTH_MSH_P;
-CLK_IN_D_0_clk_n(3) <= CLK_B_GTH_QSFP0_N;
-CLK_IN_D_0_clk_p(3) <= CLK_B_GTH_QSFP0_P;
-CLK_IN_D_0_clk_n(4) <= CLK_B_GTH_QSFP1_N;
-CLK_IN_D_0_clk_p(4) <= CLK_B_GTH_QSFP1_P;
--- SFP0, MGT128, X0Y1
-slv_fanout_mgt_qsfp0_rx_p(0) <= QSFP0_RX0_P;
-slv_fanout_mgt_qsfp0_rx_p(1) <= QSFP0_RX1_P;
-slv_fanout_mgt_qsfp0_rx_p(2) <= QSFP0_RX2_P;
-slv_fanout_mgt_qsfp0_rx_p(3) <= QSFP0_RX3_P;
-slv_fanout_mgt_qsfp0_rx_n(0) <= QSFP0_RX0_N;
-slv_fanout_mgt_qsfp0_rx_n(1) <= QSFP0_RX1_N;
-slv_fanout_mgt_qsfp0_rx_n(2) <= QSFP0_RX2_N;
-slv_fanout_mgt_qsfp0_rx_n(3) <= QSFP0_RX3_N;
-QSFP0_TX0_P <= slv_fanout_mgt_qsfp0_tx_p(0);
-QSFP0_TX1_P <= slv_fanout_mgt_qsfp0_tx_p(1);
-QSFP0_TX2_P <= slv_fanout_mgt_qsfp0_tx_p(2);
-QSFP0_TX3_P <= slv_fanout_mgt_qsfp0_tx_p(3);
-QSFP0_TX0_N <= slv_fanout_mgt_qsfp0_tx_n(0);
-QSFP0_TX1_N <= slv_fanout_mgt_qsfp0_tx_n(1);
-QSFP0_TX2_N <= slv_fanout_mgt_qsfp0_tx_n(2);
-QSFP0_TX3_N <= slv_fanout_mgt_qsfp0_tx_n(3);
--- SFP1, MGT228 X1Y1
-slv_fanout_mgt_qsfp1_rx_p(0) <= QSFP1_RX0_P;
-slv_fanout_mgt_qsfp1_rx_p(1) <= QSFP1_RX1_P;
-slv_fanout_mgt_qsfp1_rx_p(2) <= QSFP1_RX2_P;
-slv_fanout_mgt_qsfp1_rx_p(3) <= QSFP1_RX3_P;
-slv_fanout_mgt_qsfp1_rx_n(0) <= QSFP1_RX0_N;
-slv_fanout_mgt_qsfp1_rx_n(1) <= QSFP1_RX1_N;
-slv_fanout_mgt_qsfp1_rx_n(2) <= QSFP1_RX2_N;
-slv_fanout_mgt_qsfp1_rx_n(3) <= QSFP1_RX3_N;
-QSFP1_TX0_P <= slv_fanout_mgt_qsfp1_tx_p(0);
-QSFP1_TX1_P <= slv_fanout_mgt_qsfp1_tx_p(1);
-QSFP1_TX2_P <= slv_fanout_mgt_qsfp1_tx_p(2);
-QSFP1_TX3_P <= slv_fanout_mgt_qsfp1_tx_p(3);
-QSFP1_TX0_N <= slv_fanout_mgt_qsfp1_tx_n(0);
-QSFP1_TX1_N <= slv_fanout_mgt_qsfp1_tx_n(1);
-QSFP1_TX2_N <= slv_fanout_mgt_qsfp1_tx_n(2);
-QSFP1_TX3_N <= slv_fanout_mgt_qsfp1_tx_n(3);
--- P6 port 8,7,6,5, MGT228 X1Y3
-slv_fanout_mgt_p6_8765_rx_p(0) <= L8_MSH_GBT_RX_P;
-slv_fanout_mgt_p6_8765_rx_p(1) <= L7_MSH_GBT_RX_P;
-slv_fanout_mgt_p6_8765_rx_p(2) <= L6_MSH_GBT_RX_P;
-slv_fanout_mgt_p6_8765_rx_p(3) <= L5_MSH_GBT_RX_P;
-slv_fanout_mgt_p6_8765_rx_n(0) <= L8_MSH_GBT_RX_N;
-slv_fanout_mgt_p6_8765_rx_n(1) <= L7_MSH_GBT_RX_N;
-slv_fanout_mgt_p6_8765_rx_n(2) <= L6_MSH_GBT_RX_N;
-slv_fanout_mgt_p6_8765_rx_n(3) <= L5_MSH_GBT_RX_N;
-L8_MSH_GBT_TX_P <= slv_fanout_mgt_p6_8765_tx_p(0);
-L7_MSH_GBT_TX_P <= slv_fanout_mgt_p6_8765_tx_p(1);
-L6_MSH_GBT_TX_P <= slv_fanout_mgt_p6_8765_tx_p(2);
-L5_MSH_GBT_TX_P <= slv_fanout_mgt_p6_8765_tx_p(3);
-L8_MSH_GBT_TX_N <= slv_fanout_mgt_p6_8765_tx_n(0);
-L7_MSH_GBT_TX_N <= slv_fanout_mgt_p6_8765_tx_n(1);
-L6_MSH_GBT_TX_N <= slv_fanout_mgt_p6_8765_tx_n(2);
-L5_MSH_GBT_TX_N <= slv_fanout_mgt_p6_8765_tx_n(3);
 
--- P6 port 4,3,2, MGT228 X1Y2
-slv_fanout_mgt_p6_432_rx_p(0) <= L4_MSH_GBT_RX_P;
-slv_fanout_mgt_p6_432_rx_p(1) <= L3_MSH_GBT_RX_P;
-slv_fanout_mgt_p6_432_rx_p(2) <= L2_MSH_GBT_RX_P;
-slv_fanout_mgt_p6_432_rx_p(3) <= '0';
-slv_fanout_mgt_p6_432_rx_n(0) <= L4_MSH_GBT_RX_N;
-slv_fanout_mgt_p6_432_rx_n(1) <= L3_MSH_GBT_RX_N;
-slv_fanout_mgt_p6_432_rx_n(2) <= L2_MSH_GBT_RX_N;
-slv_fanout_mgt_p6_432_rx_n(3) <= '0';
-L4_MSH_GBT_TX_P <= slv_fanout_mgt_p6_432_tx_p(0);
-L3_MSH_GBT_TX_P <= slv_fanout_mgt_p6_432_tx_p(1);
-L2_MSH_GBT_TX_P <= slv_fanout_mgt_p6_432_tx_p(2);
-L4_MSH_GBT_TX_N <= slv_fanout_mgt_p6_432_tx_n(0);
-L3_MSH_GBT_TX_N <= slv_fanout_mgt_p6_432_tx_n(1);
-L2_MSH_GBT_TX_N <= slv_fanout_mgt_p6_432_tx_n(2);
+  irq <= (others => '0');
 
-CPSI_CIO_i: component CPSI_CIO
-     port map (
-      CLK_IN_D_0_clk_n(4 downto 0) => CLK_IN_D_0_clk_n(4 downto 0),
-      CLK_IN_D_0_clk_p(4 downto 0) => CLK_IN_D_0_clk_p(4 downto 0),
-      CLK_A_FPGA_DP_OUT_P       => CLK_A_FPGA_DP_OUT_P,
-      CLK_A_FPGA_DP_OUT_N       => CLK_A_FPGA_DP_OUT_N,
-      MGT_UPLINK_RECCLK_N(0)    => CLK_A_GTH_MSH_SFP1_OUT_N,
-      MGT_UPLINK_RECCLK_P(0)    => CLK_A_GTH_MSH_SFP1_OUT_P,
-      MGT_UPLINK_RX_N           => L1_GBT_MSH_SFP_RX_N,
-      MGT_UPLINK_RX_P           => L1_GBT_MSH_SFP_RX_P,
-      MGT_UPLINK_TX_N           => L1_GBT_MSH_SFP_TX_N,
-      MGT_UPLINK_TX_P           => L1_GBT_MSH_SFP_TX_P,
-      fp_lemo_in(0)             => FP_LEMO_IN_0,
-      fp_lemo_in(1)             => FP_LEMO_IN_1,
-      fp_lemo_ena_n(0)          => FP_LEMO_EN_0,
-      fp_lemo_ena_n(1)          => FP_LEMO_EN_1,
-      TRIGGER_OUT(0)            => FP_LEMO_OUT_0,
-      TRIGGER_OUT(1)            => FP_LEMO_OUT_1,
-      i_fanout_mgt_qsfp0_rx_p    => slv_fanout_mgt_qsfp0_rx_p,
-      i_fanout_mgt_qsfp0_rx_n    => slv_fanout_mgt_qsfp0_rx_n,
-      o_fanout_mgt_qsfp0_tx_p    => slv_fanout_mgt_qsfp0_tx_p,
-      o_fanout_mgt_qsfp0_tx_n    => slv_fanout_mgt_qsfp0_tx_n,
-
-      i_fanout_mgt_qsfp1_rx_p    => slv_fanout_mgt_qsfp1_rx_p,
-      i_fanout_mgt_qsfp1_rx_n    => slv_fanout_mgt_qsfp1_rx_n,
-      o_fanout_mgt_qsfp1_tx_p    => slv_fanout_mgt_qsfp1_tx_p,
-      o_fanout_mgt_qsfp1_tx_n    => slv_fanout_mgt_qsfp1_tx_n,
-
-      i_fanout_mgt_p6_8765_rx_p  => slv_fanout_mgt_p6_8765_rx_p,
-      i_fanout_mgt_p6_8765_rx_n  => slv_fanout_mgt_p6_8765_rx_n,
-      o_fanout_mgt_p6_8765_tx_p  => slv_fanout_mgt_p6_8765_tx_p,
-      o_fanout_mgt_p6_8765_tx_n  => slv_fanout_mgt_p6_8765_tx_n,
-
-      i_fanout_mgt_p6_432_rx_p   => slv_fanout_mgt_p6_432_rx_p,
-      i_fanout_mgt_p6_432_rx_n   => slv_fanout_mgt_p6_432_rx_n,
-      o_fanout_mgt_p6_432_tx_p   => slv_fanout_mgt_p6_432_tx_p,
-      o_fanout_mgt_p6_432_tx_n   => slv_fanout_mgt_p6_432_tx_n,
-
-      param_fw_build_datetime_i_0(159 downto 0) => G_FW_BUILD_DATE_TIME,
-      param_fw_git_version_i_0(255 downto 0) => G_FW_GIT_VERSION
+  PS_i: entity work.ZynqMpPSWrapper
+    port map (
+      axi_aclk             => axiClk,
+      axi_peripheral_reset => axiRst,
+      pl_ps_irq0           => irq( 7 downto 0),
+      pl_ps_irq1           => irq(15 downto 8),
+      axi_ms               => axi_ms,
+      axi_sm               => axi_sm
     );
 
-end STRUCTURE;
+end architecture STRUCTURE;
